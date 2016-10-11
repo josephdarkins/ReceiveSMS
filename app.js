a6739c7346e09c3c063810e2eed755d978008b78
JoeDarkins <joe@fonix.com>
2016-10-11 14:34:08 +0100

    simplify URL stuff

--- app.js
+++ app.js
@@ -16,7 +16,10 @@ app.post('/messageSMS', function(req, res) {
     
     //var query = req.url.parse(req.url,true).query;
     //res.end(JSON.stringify(query));
-    console.log('input: %s', req.url);
+    console.log('URL: %s', req.url);
+    console.log('body: %s', req.body);
+    console.log('params: %s', req.params);
+    console.log('originalURL: %s', req.originalURL);
     
     res.send('You have successfully sent an SMS');
 
